f920ea3533ff41e28d46caf9abbce63b49daaac8
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 00:56:25 +0300

    Modified CW_8_4

--- level_8/CW_8_4.cpp
+++ level_8/CW_8_4.cpp
@@ -1,11 +1,11 @@
 /*
-Write a function that takes a string as input and returns the string reversed.
+You need to write a function that takes a string as input and returns the string reversed.
 
 Example:
 Input: 'world' → Output: 'dlrow'
 Input: 'word' → Output: 'drow'
 
-Напишите функцию, которая принимает строку в качестве входных данных и возвращает эту строку в перевёрнутом виде.
+Вам нужно написать функцию, которая принимает строку в качестве входных данных и возвращает эту строку в перевёрнутом виде.
 
 Пример:
 Ввод: 'world' → Вывод: 'dlrow'
