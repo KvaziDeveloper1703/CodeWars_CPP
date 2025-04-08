55602b19461b3b4e0341c7bc314c492ce040ff92
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 02:09:40 +0300

    Modified CW_8_16

--- level_8/CW_8_16.cpp
+++ level_8/CW_8_16.cpp
@@ -3,14 +3,14 @@ Write a function that converts a name into initials. The input will always be tw
 The output should be two capital letters separated by a dot.
 
 Напишите функцию, которая преобразует имя в инициалы. Входные данные всегда будут состоять из двух слов, разделённых одним пробелом.
-Выход должен содержать две заглавные буквы, разделённые точкой.
+Вывод должен содержать две заглавные буквы, разделённые точкой.
 
 https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
 */
 
 #include <iostream>
 
-std::string abbrevName(const std::string& given_name) {
+std::string abbreviate_name(const std::string& given_name) {
     size_t space_index = given_name.find(' ');
     if (space_index == std::string::npos) return "";
     
@@ -23,7 +23,7 @@ std::string abbrevName(const std::string& given_name) {
 int main() {
     std::string name = "Sam Harris";
     
-    std::cout << abbrevName(name) << std::endl;
+    std::cout << abbreviate_name(name) << std::endl;
     
     return 0;
 }
