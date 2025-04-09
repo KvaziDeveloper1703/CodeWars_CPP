9e9cde8246a254efb31b72f48ed64df9639cf412
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 14:04:59 +0300

    Modified CW_7_17

--- level_7/CW_7_17.cpp
+++ level_7/CW_7_17.cpp
@@ -1,30 +1,37 @@
 /*
-Complete the function so that it returns true if the first argument (a string) ends with the second argument (also a string). Otherwise, return false.
+Write a function that returns true if the first argument (a string) ends with the second argument (also a string). Otherwise, return false.
 
 Examples:
-solution('abc', 'bc')  # True
-solution('abc', 'd')   # False
+Input: 'abc', 'bc'
+Output: True
 
-Дополните функцию так, чтобы она возвращала true, если первая строка заканчивается на вторую строку. В противном случае функция должна вернуть false.
+Input: 'abc', 'd'
+Output: False
+
+Напишите функцию, которая возвращает true, если первая строка заканчивается на вторую строку. В противном случае функция должна вернуть false.
 
 Примеры:
-solution('abc', 'bc')  # True
-solution('abc', 'd')   # False
+Ввод: 'abc', 'bc'
+Вывод: True
+
+Ввод: 'abc', 'd'
+Вывод: False
+
+https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
 */
 
 #include <iostream>
-#include <string>
 
-bool solution(const std::string& str, const std::string& ending) {
-    int str_length = str.length();
-    int ending_length = ending.length();
+bool string_ends_with(const std::string& given_string, const std::string& given_suffix) {
+    int string_length = given_string.length();
+    int suffix_length = given_suffix.length();
     
-    if (ending_length > str_length) {
+    if (suffix_length > string_length) {
         return false;
     }
     
-    for (int i = 0; i < ending_length; i++) {
-        if (str[str_length - ending_length + i] != ending[i]) {
+    for (int i = 0; i < suffix_length; i++) {
+        if (given_string[string_length - suffix_length + i] != given_suffix[i]) {
             return false;
         }
     }
@@ -33,8 +40,8 @@ bool solution(const std::string& str, const std::string& ending) {
 }
 
 int main() {
-    std::cout << std::boolalpha;
-    std::cout << solution("abc", "bc") << std::endl;
-    std::cout << solution("abc", "d") << std::endl;
+    std::cout << std::boolalpha; // This sets the output format for boolean values to "true" or "false" instead of 1 or 0.
+    std::cout << string_ends_with("abc", "bc") << std::endl;
+    std::cout << string_ends_with("abc", "d") << std::endl;
     return 0;
 }
