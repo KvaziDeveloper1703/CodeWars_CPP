d35fa586159a1cf0f347cab8a404647d514dee67
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 13:33:22 +0300

    Modified CW_7_14

--- level_7/CW_7_14.cpp
+++ level_7/CW_7_14.cpp
@@ -1,26 +1,29 @@
 /*
 Given two integers a and b, which can be positive or negative, find the sum of all integers between and including them. If the two numbers are equal, return either a or b.
 
-Examples:
-(1, 0) → 1 (1 + 0 = 1)
-(1, 2) → 3 (1 + 2 = 3)
-
 Даны два целых числа a и b, которые могут быть как положительными, так и отрицательными. Найдите сумму всех целых чисел между ними, включая сами a и b. Если числа равны, верните любое из них.
 
-Примеры:
-(1, 0) → 1 (1 + 0 = 1)
-(1, 2) → 3 (1 + 2 = 3)
+https://www.codewars.com/kata/55f2b110f61eb01779000053
 */
 
 #include <iostream>
 
 int get_sum(int a, int b) {
-    if (a > b) std::swap(a, b);
-    return ((b - a + 1) * (a + b)) / 2;
+    if (a > b) {
+        std::swap(a, b);
+    }
+
+    int sum = 0;
+
+    for (int i = a; i <= b; ++i) {
+        sum += i;
+    }
+
+    return sum;
 }
 
 int main() {
-    std::cout << get_sum(1, 0) << std::endl;
-    std::cout << get_sum(1, 2) << std::endl;
+    std::cout << get_sum(1, 10) << std::endl;
+    std::cout << get_sum(-5, 5) << std::endl;
     return 0;
 }
