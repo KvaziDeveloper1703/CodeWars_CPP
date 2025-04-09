2a4087b7c5d5127dbaa4f7cdaf999c70e1ac74d5
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 13:50:18 +0300

    Modified CW_7_16

--- level_7/CW_7_16.cpp
+++ level_7/CW_7_16.cpp
@@ -1,30 +1,42 @@
 /*
-Take two strings s1 and s2, which contain only lowercase letters from a to z. Return a new string that is sorted in ascending alphabetical order and contains only distinct letters, each appearing only once. The result should include letters from both s1 and s2.
+Write a function that takes two strings s1 and s2, which contain only lowercase letters from a to z. The function should return a new string sorted in ascending alphabetical order and containing only distinct letters, each appearing only once. The resulting string should include letters from both s1 and s2.
 
-Example:
-a = "xyaabbbccccdefww"
-b = "xxxxyyyyabklmopq"
-longest(a, b) → "abcdefklmopqwxy"
+Напишите функцию, которая принимает две строки s1 и s2, содержащие только строчные буквы от a до z. Функция должна вернуть новую строку, отсортированную в алфавитном порядке по возрастанию и содержащую только уникальные буквы, каждая из которых встречается в результате только один раз. Итоговая строка должна включать буквы из обеих строк s1 и s2.
 
-Даны две строки s1 и s2, содержащие только строчные буквы от a до z. Верните новую строку, отсортированную в алфавитном порядке по возрастанию, содержащую только уникальные буквы – каждая буква должна встречаться в результате только один раз. В итоговой строке должны быть буквы из s1 и s2.
-
-Примеры:
-a = "xyaabbbccccdefww"
-b = "xxxxyyyyabklmopq"
-longest(a, b) → "abcdefklmopqwxy"
+https://www.codewars.com/kata/5656b6906de340bd1b0000ac
 */
 
 #include <iostream>
 #include <string>
 #include <set>
 
-std::string longest(const std::string& s1, const std::string& s2) {
-    std::set<char> unique_chars(s1.begin(), s1.end());
-    unique_chars.insert(s2.begin(), s2.end());
-    return {unique_chars.begin(), unique_chars.end()};
+std::string sorted_unique_letters(const std::string& given_string_1, const std::string& given_string_2) {
+    std::set<char> unique_characters;
+
+    for (char character : given_string_1) {
+        unique_characters.insert(character);
+    }
+
+    for (char character : given_string_2) {
+        unique_characters.insert(character);
+    }
+
+    std::string result;
+
+    for (char character : unique_characters) {
+        result += character;
+    }
+
+    return result;
 }
 
 int main() {
-    std::cout << longest("xyaabbbccccdefww", "xxxxyyyyabklmopq") << std::endl;
+    std::string string_1 = "xyaabbbccccdefww";
+    std::string string_2 = "xxxxyyyyabklmopq";
+
+    std::string result = sorted_unique_letters(string_1, string_2);
+
+    std::cout << "Result: " << result << std::endl;
+    
     return 0;
 }
