18d99eaa039d5b7e1e6ac7e7f3712cdd46b11c56
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 13:16:51 +0300

    Modified CW_7_11

--- level_7/CW_7_11.cpp
+++ level_7/CW_7_11.cpp
@@ -1,16 +1,18 @@
 /*
-Your task is to return the length of the shortest word(s) in the string.
+Write a function that return the length of the shortest word(s) in the string.
 It is guaranteed that the string will never be empty, and you do not need to account for different data types.
 
-Ваша задача вернуть длину самого короткого слова(слов) в строке.
+Напишите функцию, которая возвращает длину самого короткого слова(слов) в строке.
 Гарантируется, что строка никогда не будет пустой, и нет необходимости учитывать разные типы данных.
+
+https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
 */
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-int find_shortest_word_length(const string& given_string) {
+int find_the_shortest_word_length(const string& given_string) {
     int min_length = given_string.length();
     int current_length = 0;
     
@@ -34,6 +36,6 @@ int find_shortest_word_length(const string& given_string) {
 
 int main() {
     string input = "The quick brown fox jumps over the lazy dog";
-    cout << "Length of the shortest word: " << find_shortest_word_length(input) << endl;
+    cout << "Length of the shortest word: " << find_the_shortest_word_length(input) << endl;
     return 0;
 }
