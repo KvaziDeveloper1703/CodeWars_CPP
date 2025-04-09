a7b1bcb07669a30014a61b5b53f99617626ea022
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 12:50:03 +0300

    Modified CW_8_20

--- level_8/CW_8_20.cpp
+++ level_8/CW_8_20.cpp
@@ -1,8 +1,8 @@
 /*
-Create a function that accepts a parameter representing a name and returns the message:
+Write a function that accepts a parameter representing a name and returns the message:
 "Hello, <name> how are you doing today?"
 
-Создайте функцию, которая принимает параметр, представляющий имя, и возвращает сообщение:
+Напишите функцию, которая принимает параметр, представляющий имя, и возвращает сообщение:
 "Hello, <имя> how are you doing today?"
 
 https://www.codewars.com/kata/55a70521798b14d4750000a4
